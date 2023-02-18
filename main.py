1311ad9ca08f1f53a4d81ba0e785de3e2806ca9c
ElijahWong <wongwenwei@live.com>
2023-02-18 09:55:52 +0800

    added utf-8 encoding

--- main.py
+++ main.py
@@ -6,9 +6,29 @@ with open('config.json', 'r') as f:
     config = json.load(f)
     language=config['lang']
     doc_path=config['doc_path']
-with open(doc_path, 'r') as f:
+    tld=config['tld']
+with open(doc_path, 'r',encoding='utf-8') as f:
+    #read the file with utf-8 encoding
     myText = f.read()
+    print(myText)
 
-myobj = gTTS(text=myText, lang=language, slow=False)
+
+# a function that will divide up a long text into smaller chunks
+def partition_text(text):
+    # split the text into smaller chunks
+    # each chunk is a list of sentences
+    chunks = []
+    chunk = []
+    for sentence in text.split('.'):
+        if len(chunk) < 10:
+            chunk.append(sentence)
+        else:
+            chunks.append(chunk)
+            chunk = [sentence]
+    chunks.append(chunk)
+    return chunks
+
+
+myobj = gTTS(text=myText, lang=language, tld=tld,slow=False)
   
 myobj.save("output/converted.mp3")
